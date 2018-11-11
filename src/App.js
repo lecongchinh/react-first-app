5c2115c9f7e8eace9daec8ebd4c50887e9d349d5
lecongchinh <lecongchinh.ptit@gmail.com>
2018-11-11 22:54:49 +0700

    chinh

--- src/App.js
+++ src/App.js
@@ -1,28 +1,58 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import TodoList from './TodoList'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = { items: [], text: ''};
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.removeTodo = this.removeTodo.bind(this);
+    }
+    render() {
+        return(
+            <div>
+                <h3>TODO</h3>
+                <TodoList items={this.state.items} removeTodo={this.removeTodo} />
+                <form onSubmit={this.handleSubmit}>
+                    <input
+                        id="new-todo"
+                        onChange={this.handleChange}
+                        value={this.state.text}
+                    />
+                    <button>
+                        Add #{this.state.items.length + 1}
+                    </button>
+                </form>
+            </div>
+        );
+    }
+
+    removeTodo(item) {
+        this.setState({
+            items: this.state.items.filter(element => element!==item)
+        })
+    }
+
+    handleChange(e) {
+        this.setState({text: e.target.value});
+    }
+    handleSubmit(e) {
+        e.preventDefault();
+        if(!this.state.text.length) {
+            return;
+        }
+        const newItem = {
+            text: this.state.text,
+            id: Date.now()
+        };
+        this.setState(state => ({
+            items: state.items.concat(newItem),
+            text: ''
+        }));
+    }
 }
+
 
 export default App;
